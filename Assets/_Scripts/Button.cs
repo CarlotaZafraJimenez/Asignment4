78c173ee2f4f4acdf43b7be4549aa51996012ae6
CarlotaZafraJimenez <czafrajim@ltu.edu>
2025-11-20 03:59:25 +0100

    Tut 1 and 2

---
+++ Assets/_Scripts/Button.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public bool Triggered = false;
+    public Mover TriggeredObject;  
+    public Sprite ButtonPressed;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            TriggeredObject.Moving = true;
+        }
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr.sprite = ButtonPressed;
+    }
+}
---
+++ Assets/_Scripts/MoverTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public bool Triggered = false; 
+    public Mover TriggeredObject;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            TriggeredObject.Moving = true;
+        }
+                    
+    }
+}
